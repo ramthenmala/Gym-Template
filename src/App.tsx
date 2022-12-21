bb8e7fe87251456403bd39b29a40c4effdbe6ce1
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2022-12-21 19:58:42 +0000

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <div className="app"> 
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
